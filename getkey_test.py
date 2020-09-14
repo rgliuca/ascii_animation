b78da2a5aead366d386c4948cb9bb8803c649490
rgliuca <44307228+rgliuca@users.noreply.github.com>
2020-09-13 17:47:43 -0700

    Add files via upload

---
+++ getkey_test.py
@@ -0,0 +1,17 @@
+from getkey import getch
+
+while True:
+  c=getch()
+#  print(type(c))
+  print(c[0], ":", chr(c[0]))
+  if c==b'\x03':
+    break
+
+  if c[0]==72:
+    print("up pressed")
+  elif c[0]==75:
+    print("left pressed")
+
+    
+
+
