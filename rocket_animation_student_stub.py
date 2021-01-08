43f814ef0e3ef218ca66f9c109e085cea1a8a398
rgliuca <44307228+rgliuca@users.noreply.github.com>
2021-01-07 16:34:01 -0800

    Update rocket_animation_student_stub.py
    added support to clear screen in unix/mac

--- rocket_animation_student_stub.py
+++ rocket_animation_student_stub.py
@@ -1,7 +1,20 @@
 import os, sys
 from time import sleep
 from colorama import Fore, init
-  
+from platform   import system as system_name  # Returns the system/OS name
+from subprocess import call   as system_call  # Execute a shell command
+
+def clear_screen():
+    """
+    Clears the terminal screen.
+    """
+
+    # Clear screen command as function of OS
+    command = 'cls' if system_name().lower()=='windows' else 'clear'
+
+    # Action
+    system_call([command])
+
 init() 
 
 rocket=['     |      ',
