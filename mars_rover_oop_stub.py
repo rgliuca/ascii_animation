65150d22067741edc1f6651a1ea2ca443da5bca1
rgliuca <44307228+rgliuca@users.noreply.github.com>
2021-03-29 16:10:02 -0700

    Update mars_rover_oop_stub.py

--- mars_rover_oop_stub.py
+++ mars_rover_oop_stub.py
@@ -3,134 +3,70 @@ import os, sys
 import random
 from time import sleep
 from colorama import Fore, Back, Style, init 
-from enum import Enum
 
-ROVER_ROW = 40
-ROVER_COL = 40
-SCREEN_HEIGHT = 50
-SCREEN_WIDTH = 100
-TEXT_POS = '\033[{};{}H'
+init()  
 
-ROVER_IMAGE = [ 'Ö▓▓▓▓▓Ö',
-                ' ▓▓░▓▓ ',
-                ' ▓░Ö░▓ ',
-                'Ö▓▓▓▓▓Ö']
-ROVER_COLOR = Fore.RED
+BOULDER_IMAGE = [' O ',
+                 'OOO',
+                 ' O ']
 
-MISSILE_IMAGE = ['|'] 
-MISSILE_COLOR = Fore.GREEN
-
-BOULDER_IMAGE = [ ' O ',
-                  'OOO',
-                  ' O ']
-
-class Direction(Enum):
-    UP = 1
-    DOWN = 2
-    LEFT = 3
-    RIGHT =4
-
-
-class Missile:
-    color = MISSILE_COLOR
-    image = MISSILE_IMAGE
-
-    def __init__(self, pos, pos_limits = {'min_row': 2, 
-                                          'max_row': ROVER_ROW,
-                                          'min_col': 1,
-                                          'max_col': SCREEN_WIDTH}):
-        pass
-
-    def draw(self):
-        pass
-
-    def clear(self):
-        pass
-
-
-    def move(self):
-        pass
-
-    def get_position(self):
-        pass
-
- 
-    def is_done(self):
-        pass
-
-
-class Rover:
-    image = ROVER_IMAGE
-    color = ROVER_COLOR
-
-    def __init__(self, rover_row=ROVER_ROW, rover_col=ROVER_COL): 
-        # self._row = rover_row
-        pass
-
-
-    def draw(self):
-        # draw the rover at the correct location (self._row, self._col)
-        pass
-
-
-    def clear(self):
-        # clears the rover from the screen
-        # print("          ")  # print however many lines to clear the rover from the screen
-        pass
-
-
-    def move(self, direction):
-        # is going to 
-        # 1. call clear() to clear the rover from the screen
-        # 2. change the rover's position (left, right, up, down) make changes to self._row or self._col
-        # 3. call draw() to print the rover in the new position
-        pass
-
-    def has_collided(self, other):
-        pass
-
-
-    def get_position(self):
-        pass
+ROVER_IMAGE = ['Ö▓▓▓▓▓Ö',
+               ' ▓▓░▓▓ ',
+               ' ▓░Ö░▓ ',
+               'Ö▓▓▓▓▓Ö']
     
+rover_row = 15
+rover_col = 20
 
-    def get_gun_position(self):
-        pass
-
-
-# for windows OS 
-if os.name =="nt": 
+# clear the screen for Linux/Mac and Windows
+if os.name == "nt":
     os.system("cls")
-    
-    import msvcrt
-    import ctypes
+else:
+    print(f"\033[2J ")
 
-    class _CursorInfo(ctypes.Structure):
-        _fields_ = [("size", ctypes.c_int),
-                    ("visible", ctypes.c_byte)]
-    ci = _CursorInfo()
-    handle = ctypes.windll.kernel32.GetStdHandle(-11)
-    ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
-    ci.visible = False
-    ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+print("\033[?25l") # Hide the cursor
 
-rover = Rover()
-rover.draw()
+try:
+    import tty, termios
+except ImportError:
+    # Probably Windows.
+    try:
+        import msvcrt
+    except ImportError:
+        # FIXME what to do on other platforms?
+        # Just give up here.
+        raise ImportError('getch not available')
+    else:
+        getch = msvcrt.getch
+else:
+    def getch():
+        """getch() -> key character
+        Read a single keypress from stdin and return the resulting character. 
+        Nothing is echoed to the console. This call will block if a keypress 
+        is not already available, but will not wait for Enter to be pressed. 
+        If the pressed key was a modifier key, nothing will be detected; if
+        it were a special function key, it may return the first character of
+        of an escape sequence, leaving additional characters in the buffer.
+        """
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+for n, line in enumerate(ROVER_IMAGE):
+    print(Fore.RED+'\033[{};{}H'.format(rover_row + n, rover_col) + line)
 
 while True:
-    if msvcrt.kbhit():
-        c=msvcrt.getch()
-        if c==b'\xe0':
-            c=msvcrt.getch()
-            if c==b'H':
-                print(Fore.GREEN+'\033[1;1H'+"up pressed       ")
-                rover.move(Direction.UP)
-            elif c==b'M':
-                rover.move(Direction.RIGHT)
-                print(Fore.GREEN+'\033[1;1H'+"right pressed    ")
-            elif c==b'K':
-                rover.move(Direction.LEFT)
-                print(Fore.GREEN+'\033[1;1H'+"left pressed     ")
-            elif c==b'P':
-                rover.move(Direction.DOWN)
-                print(Fore.GREEN+'\033[1;1H'+"down pressed     ")
+    c = getch()
+    print(c[0], ":", ord(c[0]))
+    if ord(c[0]) == 3:
+        break
+        
+    if ord(c[0]) == 49:
+        print("The '1' key was pressed")
+        # most likely the '1' key was pressed
